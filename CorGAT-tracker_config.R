d9ccd4cc19dd3fa985ce2a0b9bf3886175512a6c
F3rika <79254117+F3rika@users.noreply.github.com>
2021-06-19 00:13:43 +0200

    Add files via upload

--- CorGAT-tracker_config.R
+++ CorGAT-tracker_config.R
@@ -1,23 +1,23 @@
+#Defining path for input files
+allLin_inputFilesPath <- "C:/Users/Erika/Desktop/CNR/Projects/CorGAT/ShinyApps/CorGAT-tracker_App/CorGAT-tracker_inputTables/allLin/"
+specialLin_inputFilesPath <- "C:/Users/Erika/Desktop/CNR/Projects/CorGAT/ShinyApps/CorGAT-tracker_App/CorGAT-tracker_inputTables/SpecialLin/"
+mut_inputFilesPath <- "C:/Users/Erika/Desktop/CNR/Projects/CorGAT/ShinyApps/CorGAT-tracker_App/CorGAT-tracker_inputTables/Mut/"
+
 #Defining max value for "Weeks range" widget
-maxWeek <- 70
+maxWeek <- ncol(read.table(paste0(allLin_inputFilesPath, "Epiweek.","Italy",".csv"), sep = " ", check.names = F))
 
 #Defining choices for "Country" widget
-countryList <- list("Australia"="Australia",
-                    "Brazil"="Brazil",
-                    "Denmark"="Denmark",
-                    "France"="France",
-                    "India"="India",
-                    "Italy"="Italy",
-                    "Spain"="Spain",
-                    "Sweden"="Sweden",
-                    "The United Kingdom"="UnitedKingdom",
-                    "United States of America"="USA"
-                    )
+countryNames <- unique(unlist(strsplit(Sys.glob(paste0(allLin_inputFilesPath, "Epiweek.*.csv")), ".", fixed = T)))
 
-#Defining path for input files
-inputFilesPath <- "F3rika/CorGAT-tracker_data/"
+countryNames <- countryNames[countryNames!="csv"&countryNames!=paste0(allLin_inputFilesPath, "Epiweek")]
 
-#Defining color palette
+countryNames <- sort(countryNames)
+
+names(countryNames) <- countryNames
+
+countryList <- as.list(countryNames)
+
+#Defining color palettes
 library(RColorBrewer)
 
 Voc <- c("B.1.1.7", "P.1", "B.1.351", "B.1.427", "B.1.429", "Others")
@@ -25,3 +25,9 @@ colVoc <- c("chartreuse", "cyan", "gold", "maroon1", "darkorchid2", "mediumblue"
 names(colVoc) <- Voc
 
 randomColors <- brewer.pal(n = 6, name = "Pastel2")
+
+Moc <- c("N501Y", "E484K", "Others")
+colMoc <- c("green", "deeppink", "mediumblue")
+names(colMoc) <- Moc
+
+mut_randomColors <- brewer.pal(n = 11, name = "Set3")
