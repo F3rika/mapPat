94e1cc68d3a5c9b8b0719c8ce26cf027dd322d35
F3rika <79254117+F3rika@users.noreply.github.com>
2023-03-27 13:57:35 +0200

    Update tracker_app.R

--- tracker_app.R
+++ tracker_app.R
@@ -1,9 +1,9 @@
 #######CorGAT-tracker App#######
 
 #Upload packages and data -----
-source("CorGAT-tracker_config.R")
-source("CorGAT-tracker_functions.R")
-source("CorGAT-tracker_ui.R")
-source("CorGAT-tracker_server.R")
+source("tracker_config.R")
+source("tracker_functions.R")
+source("tracker_ui.R")
+source("tracker_server.R")
 
 shinyApp(ui = ui, server = server)
