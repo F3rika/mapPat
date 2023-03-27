4d80b869c0d29140803fefb21767c54e5c0aa911
F3rika <79254117+F3rika@users.noreply.github.com>
2023-03-27 15:36:47 +0200

    Update and rename CorGAT-tracker_config.R to tracker_config.R

---
+++ tracker_config.R
@@ -0,0 +1,93 @@
+##################################################################
+#      DEFINING ALL THE CONFIGURATION PARAMETERS OF THE APP      #
+##################################################################
+
+#######UPLOAD REQUIRED PACKAGES#######
+library(shiny)
+library(RColorBrewer)
+library(ggplot2)
+library(pheatmap)
+library(rgeoboundaries)
+library(leaflet)
+library(htmltools)
+
+#######DEFINIG PATHS FOR INPUT FILES#######
+config_path <- "" #Insert path to Configuration input folder
+var_path <- "" #Insert path to Variants input folder
+allLin_path <- "" #Insert path to Lineages input folder
+heatChoromap_path <- "" #Insert path to Heatmap and Choropleth Map input folder
+mut_path <- "" #Insert path to Mutations input folder
+totReg_path <- "" #Insert path to Regional Total input folder
+
+#######OPENING USEFUL FILES#######
+#Opening the Countries List Tracker file. This file collects the ISO-3 codes for all the countries
+#with a total number of sequenced genomes equal or higher than 1000. By developer choice only
+#countries respecting this requirement are considered.
+countryPass_List <- scan(paste0(config_path,"countriesListTracker.txt"),
+                         what = "character",
+                         sep = "\n",
+                         comment.char = "",
+                         quote = "")
+
+#Opening the Country Regions Converter table. This table allows to associate each country name in the
+#dropdown menu to the corresponding ISO-3 codes and regions (used for the Choropleth Maps).
+countryRegionsConvertion_Table <- read.table(paste0(config_path, "countryRegionsConverter.txt"),
+                                             sep = "\t",
+                                             header = T,
+                                             check.names = F,
+                                             comment.char = "",
+                                             quote = "")
+
+#Opening the Variants Converter Tracker table. This table allows to associate each Lineage to the
+#corresponding Variant or Variant subgroup.
+variantsConvertion_Table <- read.table(paste0(config_path, "variantsConverterTracker.txt"),
+                                       sep = "\t",
+                                       header = T,
+                                       check.names = F,
+                                       comment.char = "",
+                                       quote = "")
+colnames(variantsConvertion_Table) <- c("Lin","Var","Cat","OmiGroup")
+
+#######DEFINING WIDGETS CONTENT#######
+#Defining the maximum limit of the week selection slider.
+maxWeek <- ncol(read.table(paste0(allLin_path, "Epiweek.","ITA",".csv"),
+                           sep = " ",
+                           row.names = 1,
+                           header = T,
+                           check.names = F))
+
+#Defining the content of the country selection dropdown menu. Only countries with a total number
+#of sequenced genomes equal or higher than 1000 are considered.
+countryNames <- unique(countryRegionsConvertion_Table[countryRegionsConvertion_Table$Country_ISO%in%countryPass_List,]$Country)
+
+countryISO <- unique(countryRegionsConvertion_Table[countryRegionsConvertion_Table$Country_ISO%in%countryPass_List,]$Country_ISO)
+
+names(countryISO) <- countryNames
+
+countryList <- as.list(countryISO)
+
+#Defining the content of the Variant category selection dropdown menù.
+cat <- unique(variantsConvertion_Table$Cat)
+
+omiGr <- unique(variantsConvertion_Table$OmiGroup)
+
+omiGr <- omiGr[!omiGr=="None"]
+
+categoryNames <- c(cat, omiGr)
+
+names(categoryNames) <- categoryNames
+
+categoryList <- as.list(categoryNames)
+
+categoryList <- append(categoryList, list(All = "All"))
+
+#######DEFINING COLOR PALETTES#######
+#Defining the color palettes characterizing the color theme of each tab in the App.
+#Variants Tab Theme.
+varTheme <- brewer.pal(9, "YlOrRd")
+
+#Lineages Tab Theme.
+allLinTheme <- brewer.pal(9, "YlGnBu")
+
+#Mutations Tab Theme.
+mutTheme <- brewer.pal(9, "PuBuGn")
