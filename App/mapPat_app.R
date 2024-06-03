73a376594153162e6af4b2247225166da0e7fef3
F3rika <79254117+F3rika@users.noreply.github.com>
2024-06-03 13:49:37 +0200

    Update mapPat_app.R

--- App/mapPat_app.R
+++ App/mapPat_app.R
@@ -3,9 +3,9 @@
 ##############################
 
 #Upload packages and data -----
-source("../Scripts/mapPat_config.R")
-source("../Scripts/mapPat_functions.R")
-source("../Scripts/mapPat_ui.R")
-source("../Scripts/mapPat_server.R")
+source("./Scripts/mapPat_config.R")
+source("./Scripts/mapPat_functions.R")
+source("./Scripts/mapPat_ui.R")
+source("./Scripts/mapPat_server.R")
 
 shinyApp(ui = ui, server = server)
