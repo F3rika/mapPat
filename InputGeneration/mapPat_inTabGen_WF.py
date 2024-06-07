a77fba87378f05577ca4b3b20e15b7ded98c0ea5
F3rika <79254117+F3rika@users.noreply.github.com>
2024-06-07 15:21:52 +0200

    Update mapPat_inTabGen_WF.py

--- InputGeneration/mapPat_inTabGen_WF.py
+++ InputGeneration/mapPat_inTabGen_WF.py
@@ -26,8 +26,11 @@ def main():
      parser=argparse.ArgumentParser()
      
      parser.add_argument('-i', '--input_file', help='Input file. Metadata dowloaded from either GISAID or Nexstrain.')
+     parser.add_argument('-pc', '--path_config', default='./Config', help='Path to the "Config" folder which collects required configuration files.')
+     parser.add_argument('-ps', '--path_scripts', default='./Scripts', help='Path to the "Scripts" folder which collects required scripts.')
      parser.add_argument('-s', '--seq', required=False, help='Sequencing file in FASTA format.')
-     parser.add_argument('-p', '--pathogen', choices=['SARS-CoV-2', 'mPox'], help='Name of the pathogen of interest. Allowed values: SARS-CoV-2, mPox.')
+     parser.add_argument('-rf', '--refSeq', required=False, help='Reference sequence in FASTA format.')
+     parser.add_argument('-p', '--pathogen', help='Name of the pathogen of interest.')
      parser.add_argument('-db', '--database', choices=['GISAID', 'Nextstrain'], help='Name of the database from which metadata are dowloaded. Allowed values: GISAID, Nextstrain.')
      parser.add_argument('-o', '--output_file', help='Basename of the compressed file where mapPat outputs are stored.')
      
@@ -37,19 +40,19 @@ def main():
          command('mkdir %s_mapPatInterOut'%(inputs.output_file))
          command('mkdir %s_mapPatOut'%(inputs.output_file))
          
-         command('perl ./Scripts/GisaidToHaploCoV.pl --metadata %s --outfile SARS-CoV-2.HaploCoV'%(inputs.input_file))
+         command('perl %s/GisaidToHaploCoV.pl --metadata %s --outfile SARS-CoV-2.HaploCoV'%(inputs.path_scripts, inputs.input_file))
          
-         command('perl ./Scripts/countMlin.pl SARS-CoV-2.HaploCoV > linDefMut.csv')
+         command('perl %s/countMlin.pl SARS-CoV-2.HaploCoV > linDefMut.csv'%(inputs.path_scripts))
          
-         command('perl ./Scripts/BuildTables.pl SARS-CoV-2.HaploCoV SI')
+         command('perl %s/BuildTables.pl SARS-CoV-2.HaploCoV SI'%(inputs.path_scripts))
          
          build_coutriesListTracker('inTab_avCheck_tmp.txt')
          
-         command('python3 ./Scripts/LinVar_ConvTabBuilder.py -rv ./Config/LinVar_AssocTab.txt -rs ./Config/LinVBM_AssocTab.txt -ra ./Config/SARS-CoV-2_LineagesAliases.json -t %s -o LinVar_ConvTabTracker.txt'%(inputs.input_file))
+         command('python3 %s/LinVar_ConvTabBuilder.py -rv LinVar_AssocTab.txt -rs LinVBM_AssocTab.txt -ra SARS-CoV-2_LineagesAliases.json -t %s -o LinVar_ConvTabTracker.txt'%(inputs.path_scripts, inputs.input_file))
          
-         command('python3 ./Scripts/Var_CountsTabBuilder.py -rc countriesListTracker.txt -rv LinVar_ConvTabTracker.txt')
+         command('python3 %s/Var_CountsTabBuilder.py -rc countriesListTracker.txt -rv LinVar_ConvTabTracker.txt'%(inputs.path_scripts))
          
-         command('python3 ./Scripts/InAvailability_ConfigTabUpdater.py -iav inTab_avCheck_tmp.txt -rc countriesListTracker.txt -p SARS-CoV-2')
+         command('python3 %s/InAvailability_ConfigTabUpdater.py -iav inTab_avCheck_tmp.txt -rc countriesListTracker.txt -p SARS-CoV-2'%(inputs.path_scripts))
          
          command('mv -t ./%s_mapPatInterOut linDefMut.csv SARS-CoV-2.HaploCoV inTab_avCheck_tmp.txt'%(inputs.output_file))
          command('mv -t ./%s_mapPatOut Epiweek.*.csv *_muts_perLin.csv HeatmapRegLin_*.csv Total_*_regions.csv countriesListTracker.txt inTab_avCheck.txt LinVar_ConvTabTracker.txt'%(inputs.output_file))
@@ -57,21 +60,21 @@ def main():
          command('tar -czvf %s_mapPatInterOut.tar.gz ./%s_mapPatInterOut --remove-files'%(inputs.output_file, inputs.output_file))
          command('tar -czvf %s_mapPatOut.tar.gz ./%s_mapPatOut --remove-files'%(inputs.output_file, inputs.output_file))
      
-     elif inputs.database=='Nextstrain' and inputs.pathogen=='mPox':
+     elif inputs.database=='Nextstrain' and inputs.pathogen!='SARS-CoV-2':
          command('mkdir %s_mapPatInterOut'%(inputs.output_file))
          command('mkdir %s_mapPatOut'%(inputs.output_file))
          
-         command('perl ./Scripts/addToTableNextstrain.pl --metadata %s --seq %s --outfile MpoxTable.HaploCoV --nproc 18 --ref ./Config/mPox_reference.fasta'%(inputs.input_file, inputs.seq))
+         command('perl %s/addToTableNextstrain.pl --metadata %s --seq %s --outfile %sTable.HaploCoV --nproc 18 --ref %s'%(inputs.path_scripts, inputs.input_file, inputs.seq, inputs.pathogen, inputs.refSeq))
          
-         command('perl ./Scripts/countMlin.pl MpoxTable.HaploCoV > linDefMut.csv')
+         command('perl %s/countMlin.pl %sTable.HaploCoV > linDefMut.csv'%(inputs.path_scripts, inputs.pathogen))
          
-         command('perl ./Scripts/BuildTables.pl MpoxTable.HaploCoV NO')
+         command('perl %s/BuildTables.pl %sTable.HaploCoV NO'%(inputs.path_scripts, inputs.pathogen))
          
          build_coutriesListTracker('inTab_avCheck_tmp.txt')
          
-         command('python3 ./Scripts/InAvailability_ConfigTabUpdater.py -iav inTab_avCheck_tmp.txt -p mPox')
+         command('python3 %s/InAvailability_ConfigTabUpdater.py -iav inTab_avCheck_tmp.txt -p mPox'%(inputs.path_scripts))
          
-         command('mv -t ./%s_mapPatInterOut MpoxTable.HaploCoV linDefMut.csv inTab_avCheck_tmp.txt'%(inputs.output_file))
+         command('mv -t ./%s_mapPatInterOut %sTable.HaploCoV linDefMut.csv inTab_avCheck_tmp.txt'%(inputs.output_file, inputs.pathogen))
          command('mv ./Tgenomes ./%s_mapPatInterOut'%(inputs.output_file))
          command('mv -t ./%s_mapPatOut Epiweek.*.csv *_muts_perLin.csv HeatmapRegLin_*.csv Total_*_regions.csv countriesListTracker.txt inTab_avCheck.txt'%(inputs.output_file))
          
