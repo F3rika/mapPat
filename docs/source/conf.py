4e9f6938604fe787123e5bdbfb1ce148465ffbc1
F3rika <79254117+F3rika@users.noreply.github.com>
2024-06-19 15:49:44 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,7 @@ release = '1.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme'
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
