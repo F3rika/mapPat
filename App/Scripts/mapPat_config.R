d8c585df13e7d5aef60bf99fbe3e4c111b6624e6
F3rika <79254117+F3rika@users.noreply.github.com>
2024-11-22 10:58:07 +0100

    Update mapPat_config.R

--- App/Scripts/mapPat_config.R
+++ App/Scripts/mapPat_config.R
@@ -9,6 +9,13 @@ installedPackages <- rownames(installed.packages())
 
 isInstalled <- requiredPackages%in%installedPackages
 
+#Setting the CRAN mirror from which missing packages are installed.
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org"
+  options(repos = r)
+})
+
 #Installing missing packages.
 if (sum(isInstalled)<7) {
   
@@ -27,53 +34,47 @@ library(rgeoboundaries)
 library(leaflet)
 library(htmltools)
 
-#######DEFINING PATHS FOR INPUT FILES#######
-inputs_path <- "./Input/"
-config_path <- paste0(inputs_path,"Config/")
-var_path <- paste0(inputs_path,"Var/")
-allLin_path <- paste0(inputs_path,"allLin/")
-heatChoromap_path <- paste0(inputs_path,"HeatChoromap/")
-mut_path <- paste0(inputs_path,"Mut/")
-totReg_path <- paste0(inputs_path,"totReg/")
+#######CHECK DATASET UPDATE#######
+#Defining path to Datasets folder.
+datasets_path <- "./Datasets/"
 
-#######UNZIPPING INPUT FILES#######
-#Unzipping all the input files in the inputData folder.
-zippedFiles <- list.files(path = inputs_path,
-                          pattern = "*\\.tar\\.gz$")
+#Dowload the most recent copy of the Input Tables Updated Availability configuration File.
+download.file("https://raw.githubusercontent.com/F3rika/mapPat/refs/heads/mapPat_Current/App/Datasets/mapPat_inTabUpdates_Availability.txt", destfile = paste0(datasets_path, "mapPat_inTabUpdates_Availability.txt"))
 
-if (length(zippedFiles)>0) {
+#Opening the Input Tables Updated Availability configuration File.
+inTabAvail_Table <- read.table(paste0(datasets_path, "mapPat_inTabUpdates_Availability.txt"),
+                               sep = "\t",
+                               header = T,
+                               check.names = F,
+                               comment.char = "",
+                               quote = "",
+                               fileEncoding = "UTF-8")
+
+#######DEFINING DEFAULT CONFIGURATION#######
+#Downloading the default dataset for the App and unzipping it if required.
+checkUpd <- list.files(datasets_path)
+checkDef<- inTabAvail_Table[1,]$UpdateFile
+
+if (!(checkDef %in% checkUpd)) {
   
-  toUnzip <- sapply(inputs_path, paste0, zippedFiles)
+  download.file(inTabAvail_Table[1,]$UpdateURL, destfile = paste0(datasets_path, inTabAvail_Table[1,]$UpdateFile))
   
-  sapply(toUnzip, untar, compressed = TRUE, exdir = inputs_path)
+  untar(paste0(datasets_path, inTabAvail_Table[1,]$UpdateFile), exdir = paste0(datasets_path, inTabAvail_Table[1,]$UpdateFolder))
   
 }
 
-#######OPENING USEFUL FILES#######
+#Defining paths for input files.
+inputs_pathDEF <- paste0(datasets_path, inTabAvail_Table[1,]$UpdatePath)
+config_pathDEF <- paste0(inputs_pathDEF,"Config/")
+var_pathDEF <- paste0(inputs_pathDEF,"Var/")
+allLin_pathDEF <- paste0(inputs_pathDEF,"allLin/")
+heatChoromap_pathDEF <- paste0(inputs_pathDEF,"HeatChoromap/")
+mut_pathDEF <- paste0(inputs_pathDEF,"Mut/")
+totReg_pathDEF <- paste0(inputs_pathDEF,"totReg/")
+
 #Opening the Pathogen Selection Configuration Table. This table allows to associate each pathogen
 #with associated information used for data default and widgets definition.
-pathogenSelConf_Table <- read.table(paste0(config_path, "PathogenSelection_ConfigTab.txt"),
-                                    sep = "\t",
-                                    header = T,
-                                    check.names = F,
-                                    comment.char = "",
-                                    quote = "",
-                                    fileEncoding = "UTF-8")
-
-#Opening the Country ISO-ADM Association table. This table allows to associate each country name in the
-#dropdown menu to the corresponding ISO-3 codes and ADM level (used for the Choropleth Maps).
-countryISOADMConvertion_Table <- read.table(paste0(config_path, "CountryISOADM_AssocTab.txt"),
-                                             sep = "\t",
-                                             header = T,
-                                             check.names = F,
-                                             comment.char = "",
-                                             quote = "",
-                                             fileEncoding = "UTF-8")
-
-#Opening the Lineage to Variant Tracker Conversion table. This table allows to associate each Lineage to the
-#corresponding Variant, Status (according to WHO risk classification) and if it is currently under monitoring
-#(VBM).
-variantsConvertion_Table <- read.table(paste0(config_path, "LinVar_ConvTabTracker.txt"),
+pathogenSelConf_TableDEF <- read.table(paste0(config_pathDEF, "PathogenSelection_ConfigTab.txt"),
                                        sep = "\t",
                                        header = T,
                                        check.names = F,
@@ -81,18 +82,47 @@ variantsConvertion_Table <- read.table(paste0(config_path, "LinVar_ConvTabTracke
                                        quote = "",
                                        fileEncoding = "UTF-8")
 
-#######DEFINING WIDGETS CONTENT#######
-#Defining the content of the Pathogen selection drop down menu.
-pathogenNames <- pathogenSelConf_Table$PathogenName
+#Opening the Country ISO-ADM Association table. This table allows to associate each country name in the
+#dropdown menu to the corresponding ISO-3 codes and ADM level (used for the Choropleth Maps).
+countryISOADMConvertion_TableDEF <- read.table(paste0(config_pathDEF, "CountryISOADM_AssocTab.txt"),
+                                               sep = "\t",
+                                               header = T,
+                                               check.names = F,
+                                               comment.char = "",
+                                               quote = "",
+                                               fileEncoding = "UTF-8")
 
-pathogenAbbr <- pathogenSelConf_Table$PathogenAbbr
+#Opening the Lineage to Variant Tracker Conversion table. This table allows to associate each Lineage to the
+#corresponding Variant, Status (according to WHO risk classification) and if it is currently under monitoring
+#(VBM).
+variantsConvertion_TableDEF <- read.table(paste0(config_pathDEF, "LinVar_ConvTabTracker.txt"),
+                                          sep = "\t",
+                                          header = T,
+                                          check.names = F,
+                                          comment.char = "",
+                                          quote = "",
+                                          fileEncoding = "UTF-8")
+
+#Defining the content of the Dataset selection drop down menu.
+datasetNames <- inTabAvail_Table$UpdateName
+
+datasetFile <- inTabAvail_Table$UpdateFolder
+
+names(datasetFile) <- datasetNames
+
+datasetList <- as.list(datasetFile)
+
+#Defining the content of the Pathogen selection drop down menu.
+pathogenNames <- pathogenSelConf_TableDEF$PathogenName
+
+pathogenAbbr <- pathogenSelConf_TableDEF$PathogenAbbr
 
 names(pathogenAbbr) <- pathogenNames
 
-pathogenList <- as.list(pathogenAbbr)
+pathogenListDEF <- as.list(pathogenAbbr)
 
 #Defining the content of the Variant category selection drop down menu.
-status <- unique(variantsConvertion_Table$Status)
+status <- unique(variantsConvertion_TableDEF$Status)
 
 status <- append(status, c("VBM", "All"))
 
@@ -100,7 +130,7 @@ status <- status[order(status)]
 
 names(status) <- status
 
-statusList <- as.list(status)
+statusListDEF <- as.list(status)
 
 #######DEFINING COLOR PALETTES#######
 #Defining the color palettes characterizing the color theme of each tab in the App.
